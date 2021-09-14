79a4829c59b41faa491d5a360816ececb2424546
huckjoo <huckjoo96@naver.com>
2021-09-14 21:06:35 +0900

    Link flask and mongoDB

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myweb"
+mongo = PyMongo(app)
 
-@app.route('/')
+
+@app.route("/", methods=["GET", "POSt"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        title = request.form.get("title")
+        description = request.form.get("description")
+        writer = request.form.get("writer")
+        board = mongo.db.board
+        post = {"title": title, "writer": writer, "description": description}
+        board.insert_one(post)
+        return render_template("success.html", name=post["writer"])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
