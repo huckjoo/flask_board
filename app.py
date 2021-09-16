3af37b1d78b6ef23633da847e8335721e6655a56
huckjoo <huckjoo96@naver.com>
2021-09-16 21:20:14 +0900

    Create board

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from bson.objectid import ObjectId
+from flask import Flask, render_template, request, redirect, url_for, abort
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -14,10 +15,32 @@ def index():
         title = request.form.get("title")
         description = request.form.get("description")
         writer = request.form.get("writer")
+        # board라는 Collection에 접근 없으면 생성
         board = mongo.db.board
         post = {"title": title, "writer": writer, "description": description}
-        board.insert_one(post)
-        return render_template("success.html", name=post["writer"])
+        db_post = board.insert_one(post)
+        print(db_post)
+        return redirect(url_for("board", idx=db_post.inserted_id))
+
+
+@app.route("/board")
+def board():
+    if request.args.get("idx"):
+        idx = request.args.get("idx")
+        board = mongo.db.board
+        print("모든 아이템", board.find({}))
+        if board.find_one({"_id": ObjectId(idx)}):
+            myweb_data = board.find_one({"_id": ObjectId(idx)})
+            if myweb_data != "":
+                db_data = {
+                    "id": myweb_data.get("_id"),
+                    "title": myweb_data.get("title"),
+                    "description": myweb_data.get("description"),
+                    "writer": myweb_data.get("writer"),
+                }
+                return render_template("board.html", db_data=db_data)
+            return abort(404)
+        return abort(404)
 
 
 if __name__ == "__main__":
