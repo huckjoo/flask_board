457a21ee013a79ff02e3d04d2c23ed40b1cec58b
huckjoo <huckjoo96@naver.com>
2021-09-17 21:28:43 +0900

    Read MongoDB data to Client

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from bson.objectid import ObjectId
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -19,8 +19,16 @@ def index():
         board = mongo.db.board
         post = {"title": title, "writer": writer, "description": description}
         db_post = board.insert_one(post)
-        print(db_post)
-        return redirect(url_for("board", idx=db_post.inserted_id))
+        return redirect(url_for("view_board"))
+
+
+@app.route("/view")
+def view_board():
+    board = mongo.db.board
+    board_components = list(board.find({}, {"_id": False}))
+    # mongoDB에서 전체를 불러와 list로 저장하는데
+    # _id false는 도대체 왜 있는거냐? id가 없으니까
+    return render_template("boardview.html", board_components=board_components)
 
 
 @app.route("/board")
